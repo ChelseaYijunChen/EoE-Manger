f29d792776fe910a8a8b3bb5de23fd54253fc180
ChelseaYijunChen <yijun2@g.clemson.edu>
2016-11-14 01:27:50 -0500

    Add files via upload

---
+++ fatherEducationTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  fatherEducationTableViewCell.swift
+//  EoE_1
+//
+//  Created by SUN on 15/11/22.
+//  Copyright © 2015年 SUN. All rights reserved.
+//
+
+import UIKit
+
+class fatherEducationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBOutlet weak var educationLabel: UILabel!
+    
+
+}
