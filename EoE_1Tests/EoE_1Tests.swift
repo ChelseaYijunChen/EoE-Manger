b348bb926663eb4bea0522f02a56d6489ea8ad23
ChelseaYijunChen <yijun2@g.clemson.edu>
2016-11-14 01:30:12 -0500

    Add files via upload

---
+++ EoE_1Tests/EoE_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  EoE_1Tests.swift
+//  EoE_1Tests
+//
+//  Created by SUN on 15/11/13.
+//  Copyright © 2015年 SUN. All rights reserved.
+//
+
+import XCTest
+@testable import EoE_1
+
+class EoE_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
